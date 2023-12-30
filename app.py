632dffd736e4ad9360157c4aa8414c271209bc93
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2023-12-30 11:46:56 +0200

    1

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from website import create_app
-# from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-#  virtualenv venv
-# venv\Scripts\activate
-# python app.py
+
+# Running Flask steps:
+# (1) cd C:\Radiology-Department-Database-Project 
+# if you don't have a virtual environment :virtualenv venv
+# (2) venv\Scripts\activate
+# (3) python app.py
