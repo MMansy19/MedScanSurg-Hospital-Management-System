dcff533834a5d95dba072d42554ae808a02df89c
shehap <115353206+shehab400@users.noreply.github.com>
2023-12-30 03:08:53 +0200

    connecting to postgresql datbase and using sql queries instead of python ones

---
+++ Code/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+database_session = psycopg2.connect(
+#     database='project',
+#     port=5432,
+#     host='localhost',
+#     user='postgres',
+#     password="admin"
+# )
+# cursor = database_session.cursor(cursor_factory=psycopg2.extras.DictCursor)
