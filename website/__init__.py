6eedcb5438ecae3cd50d3473eb11b6196d357c88
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-01 04:36:11 +0200

    1

--- website/__init__.py
+++ website/__init__.py
@@ -2,38 +2,42 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-
+import psycopg2
+import psycopg2.extras
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
+database_session = psycopg2.connect(
+    database='postgres',
+    port=5432,
+    host='localhost',
+    user='postgres',
+    password= '132003'
+)
+cursor = database_session.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
-    from .views import views
-    from .models import Doctor
-
-
-    app.register_blueprint(views, url_prefix='/')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/database'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
 
     
-    with app.app_context():
-        db.create_all()
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    
     login_manager = LoginManager(app)
     login_manager.login_view = 'views.login'
     login_manager.init_app(app)
 
+    
+  
     @login_manager.user_loader
     def load_user(user_id):
-        return Doctor.query.get(int(user_id))
+        cursor.execute(f'SELECT * FROM doctor WHERE user_id ={user_id}')
+        doctor = cursor.fetchall()
+        return doctor
+    
     return app
 
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
-        
-        
--- website/views.py
+++ website/views.py
@@ -4,10 +4,21 @@ import secrets
 from PIL import Image
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
-from . import db
+from . import db , cursor
 from .models import *
+import psycopg2
+import psycopg2.extras
 
 views = Blueprint('views', __name__)
+
+database_session = psycopg2.connect(
+    database='postgres',
+    port=5432,
+    host='localhost',
+    user='postgres',
+    password= '132003'
+)
+cursor = database_session.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # Utility function to save a picture
 def save_picture(form_picture):
@@ -22,93 +33,114 @@ def save_picture(form_picture):
         i.thumbnail(output_size)
         i.save(picture_path)
         return picture_fn
-    else:
+    else:   
         return 'default.jpg'
+
 
 class EditProfileForm(FlaskForm):
     full_name = StringField('Full Name', validators=[DataRequired(), Length(max=30)])
     password = StringField('Password', validators=[DataRequired(), Length(max=30)])
-    specialty = StringField('Specialty', validators=[DataRequired(), Length(max=30)])
+    specialty = StringField('Specialty')
     salary = IntegerField('Salary', validators=[DataRequired()])
     working_hours = IntegerField('Working Hours', validators=[DataRequired()])
     phone = StringField('Phone', validators=[DataRequired(), Length(max=15)])
     address = StringField('Address', validators=[DataRequired(), Length(max=100)])
     photo = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
 
-    def populate_from_doctor(self, doctor):
-        self.full_name.data = doctor.full_name
-        self.password.data = doctor.password
-        self.specialty.data = doctor.specialty
-        self.salary.data = doctor.salary
-        self.working_hours.data = doctor.working_hours
-        self.phone.data = doctor.phone
-        self.address.data = doctor.address
-
-    def update_doctor(self, doctor, photo_file):
-        doctor.full_name = self.full_name.data
-        doctor.password = self.password.data
-        doctor.specialty = self.specialty.data
-        doctor.salary = self.salary.data
-        doctor.working_hours = self.working_hours.data
-        doctor.phone = self.phone.data
-        doctor.address = self.address.data
+    def populate_from_doctor(self, doctor_id):
+        
+        # Update the Doctor information in the database
+        cursor.execute("""
+            UPDATE doctor
+            SET 
+                full_name = %s,
+                working_hours = %s,
+                salary = %s,
+                phone = %s,
+                address = %s
+            WHERE id = %s;
+        """, (self.full_name.data,
+              self.working_hours.data, self.salary.data, self.phone.data,
+              self.address.data, int(doctor_id)))
+        database_session.commit()
+    def update_doctor(self, doctor_id, photo_file):
 
         if photo_file:
-            doctor.photo = save_picture(photo_file)    
+            self.photo.data = save_picture(photo_file)    
+        
+        # Update the Doctor information in the database
+        cursor.execute("""
+            UPDATE doctor
+            SET 
+                full_name = %s,
+                specialty = %s,
+                salary = %s,
+                working_hours = %s,
+                phone = %s,
+                address = %s,
+                photo = %s
+            WHERE id = %s;
+        """, (self.full_name.data, self.specialty.data,
+              self.salary.data, self.working_hours.data, self.phone.data,
+              self.address.data, self.photo.data,(doctor_id),))
 
-
+        database_session.commit()
+        
 # Utility function to create a new patient
 def create_patient(username, name, email, password, birthdate):
     birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()
-    new_patient = Patient(username=username, name=name, email=email, password=password, birthdate=birthdate)
-    db.session.add(new_patient)
-    db.session.commit()
-
+    cursor.execute('INSERT INTO patient (user_name, full_name, email, password, birthdate) VALUES (%s, %s, %s, %s, %s)', (username, name, email, password, birthdate))
+    database_session.commit()
 # Utility function to authenticate a user and render the appropriate template
 def authenticate_user(user_type, username, password):
     if user_type == 'patient':
-        user = Patient.query.filter_by(username=username).first()
+                cursor.execute('SELECT password FROM patient WHERE user_name = %s', (username,))
+                password = cursor.fetchone()
+                if password is not None:
+                    if password == password:
+                        return render_template('index.html')
+
     elif user_type == 'doctor':
-        user = Doctor.query.filter_by(user_name=username).first()
+        doctor = cursor.execute('SELECT * FROM doctor WHERE user_name = %s', (username)).fetchone()
+        if doctor:
+            if doctor.password ==  password:
+                return render_template('doctor.html')
+
     elif user_type == 'admin':
         if username == 'admin_username' and password == 'admin_password':
             return render_template('admin.html')
 
-    if user and user.password == password:
-        if user_type == 'patient':
-            return render_template('index.html', patient=user)
-            # return redirect(url_for('views.index', patient=user))
-        elif user_type == 'doctor':
-            # return render_template('doctor.html', doctor=user)
-            return redirect(url_for('views.doctor', doctor_id=user.id))
-
-    return None
+    return render_template('login.html')
 
 # Utility function to create a new doctor
 def create_doctor(ssn, email, password, user_name, full_name):
-    existing_doctor = Doctor.query.filter_by(user_name=user_name).first()
-    if not existing_doctor:
-        new_doctor = Doctor(
-            ssn=ssn, email=email, password=password, user_name=user_name,
-            full_name=full_name, working_hours=40, salary=80000, phone='1234567890',
-            address='Some Address', specialty='Surgery/Radiology'
-        )
-        db.session.add(new_doctor)
-        db.session.commit()
+        cursor.execute('SELECT * FROM doctor WHERE user_name = %s', (user_name,))
+        existing_doctor=cursor.fetchone()
+        
+        if not existing_doctor:
+            working_hours = 40
+            salary = 80000
+            phone = '1234567890'
+            address = 'Some Address'
+            specialty= 'Radiology'
+            gender='M' 
+            cursor.execute('INSERT INTO doctor(ssn,email, password, user_name,full_name,working_hours,salary,phone,address,specialty,gender) VALUES (%s, %s ,%s,%s,%s,%s,%s,%s,%s,%s,%s)', (ssn,email,password, user_name, full_name,working_hours,salary,phone,address, specialty,gender))
+            database_session.commit()
 
 # Utility function to handle appointment creation
-def create_appointment(patient_id, doctor_id, appointment_time_str):
-    appointment_time = datetime.strptime(appointment_time_str, '%Y-%m-%dT%H:%M')
-    new_appointment = Appointment(doctor_id=doctor_id, patient_id=patient_id, appointment_time=appointment_time)
-    db.session.add(new_appointment)
-    db.session.commit()
+# def create_appointment(patient_id, doctor_id, appointment_time_str):
+#     appointment_time = datetime.strptime(appointment_time_str, '%Y-%m-%dT%H:%M')
+#     new_appointment = Appointment(doctor_id=doctor_id, patient_id=patient_id, appointment_time=appointment_time)
+#     db.session.add(new_appointment)
+#     db.session.commit()
 
 # Main route for index
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def index(patient_id):
-    patient = Patient.query.get(patient_id)
-    return render_template('index.html', patient=patient)
+    cursor.execute(f'SELECT * FROM patient WHERE ID = {patient_id};')
+    patient = cursor.fetchone()
+    return render_template('index.html', patient_id=patient_id)
 
 # Route for login
 @views.route('/login', methods=['GET', 'POST'])
@@ -140,12 +172,9 @@ def login():
 def doctor(doctor_id):
 
     # Retrieve all appointments for the specified doctor with patient information
-    appointments = (
-        db.session.query(Appointment, Patient)
-        .join(Patient, Appointment.patient_id == Patient.patient_id)
-        .filter(Appointment.doctor_id == doctor_id)
-        .all()
-    )
+    cursor.execute(f'SELECT scan.*, patient.* FROM appointment JOIN patient ON scan.patient_id = patient.ID WHERE scan.doctor_id ={doctor_id}')
+    appointments = cursor.fetchall()
+
     return render_template('doctor.html', appointments = appointments, doctor_id=doctor_id)
 
 @views.route('/patient')
@@ -154,7 +183,6 @@ def patient():
 
 @views.route('/admin', methods=['GET', 'POST'])
 def admin():
-    doctors = Doctor.query.all()
 
     if request.method == 'POST':
         create_doctor(
@@ -162,29 +190,36 @@ def admin():
             request.form['user_name'], request.form['full_name']
         )
 
+    cursor.execute('SELECT * FROM doctor')
+    doctors = cursor.fetchall()
     return render_template('admin.html', doctors=doctors)
 
 # Route for appointments
-@views.route('/appointments/<int:patient_id>', methods=['GET', 'POST'])
-def appointments(patient_id):
-    if request.method == 'POST':
-        create_appointment(
-            patient_id, request.form.get('doctor_id'), request.form.get('appointment_time')
-        )
-        return redirect(url_for('views.appointments', patient_id=patient_id))
+# @views.route('/appointments/<int:patient_id>', methods=['GET', 'POST'])
+# def appointments(patient_id):
+#     if request.method == 'POST':
+#         create_appointment(
+#             patient_id, request.form.get('doctor_id'), request.form.get('appointment_time')
+#         )
+#         return redirect(url_for('views.appointments', patient_id=patient_id))
 
-    doctors = Doctor.query.all()
-    return render_template('appointments.html', doctors=doctors, patient_id=patient_id)
+#         cursor.execute('SELECT * FROM doctor')
+#         doctors=cursor.fetchone()
+#         return render_template('appointments.html', doctors=doctors, patient_id=patient_id)
 
 # Route for editing doctor profile
 @views.route('/edit_doctor/<int:doctor_id>', methods=['GET', 'POST'])
 def edit_doctor(doctor_id):
-    doctor = Doctor.query.get(doctor_id)
+    database_session.rollback()
+    cursor.execute('SELECT * FROM doctor WHERE id = %s', (doctor_id,))
+    doctor=cursor.fetchone()
     form = EditProfileForm()
 
     if request.method == 'GET':
-        form.populate_from_doctor(doctor)
-        photo = url_for('static', filename=f'profile_pics/{doctor.photo}')
+        form.populate_from_doctor(doctor_id)
+        cursor.execute(f'SELECT photo FROM doctor WHERE ID = {doctor_id}') 
+        photo=cursor.fetchone()
+        photo = url_for('static', filename=f'profile_pics/{photo}')
         return render_template('edit_doctor.html', photo=photo, form=form, doctor=doctor)
 
     form.update_doctor(doctor, request.files.get('photo'))
@@ -194,13 +229,13 @@ def edit_doctor(doctor_id):
 # Route for deleting a doctor
 @views.route('/delete_doctor/<int:doctor_id>', methods=['POST'])
 def delete_doctor(doctor_id):
-    doctor = Doctor.query.get(doctor_id)
-    appointments = Appointment.query.filter_by(doctor_id=doctor_id).all()
+    # doctor = Doctor.query.get(doctor_id)
+    # appointments = Appointment.query.filter_by(doctor_id=doctor_id).all()
 
-    for appointment in appointments:
-        db.session.delete(appointment)
-
-    db.session.delete(doctor)
-    db.session.commit()
+    # for appointment in appointments:
+    #     db.session.delete(appointment)
+    
+    cursor.execute('DELETE FROM doctor WHERE id = %s', (doctor_id,))
+    database_session.commit()
     return redirect(url_for('views.admin'))
 
