127c248d0175cdda8e221532ed75c429f409a464
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-04 02:33:34 +0200

    ..

--- Planning/ProjectSchema.sql
+++ Planning/ProjectSchema.sql
@@ -2,47 +2,39 @@ CREATE TABLE Doctor (
     ID SERIAL PRIMARY KEY,
     SSN INT,
     email VARCHAR(100),
-    password VARCHAR(50) NOT NULL ,
     user_name VARCHAR(50) UNIQUE NOT NULL ,
+    password VARCHAR(50) NOT NULL ,
     full_name VARCHAR(50),
     working_hours INT,
     salary INT,
     phone VARCHAR(11),
     address VARCHAR(30),
+    specialty VARCHAR(100),
     gender VARCHAR(1),
     photo VARCHAR(100),
-    speciality VARCHAR(100)
+    department VARCHAR(100),
+    start_work INT,
+    end_work INT
     CONSTRAINT chk_gender CHECK(gender IN ('M','F'))
-);
+    CONSTRAINT chk_specialty CHECK (specialty IN ('Orthopedic Surgery', 'Cardiovascular Surgery', 'Neurosurgery'))
+    CONSTRAINT chk_department CHECK (department IN ('Radiology', 'Surgery'))
 
-CREATE TABLE Department(
-    ID SERIAL PRIMARY KEY,
-    name VARCHAR(9),
-    location VARCHAR(50),
-    CONSTRAINT chk_department CHECK (name IN ('Radiology', 'Surgery'))
-);
-
-CREATE TABLE WorksIn(
-    department_id INT REFERENCES Department(ID),
-    doctor_id INT REFERENCES Doctor(ID),
-    date_hired DATE
 );
 
 CREATE TABLE Patient (
     ID SERIAL PRIMARY KEY,
     SSN VARCHAR(14),
     email VARCHAR(100) NOT NULL,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(100) NOT NULL ,
+    user_name VARCHAR(100) UNIQUE NOT NULL,
+    pass_word VARCHAR(100) NOT NULL ,
     full_name VARCHAR(100),
     birthdate DATE
 );
 
 CREATE TABLE Scan
 (
-    scan_id       serial
-        primary key,
-    doctor_id     integer constraint patient_id___fk references patient,
+    scan_id SERIAL PRIMARY KEY,
+    doctor_id INTEGER REFERENCES doctor(ID),
     price         integer,
     machine       varchar(100) not null,
     category      varchar(100) not null,
@@ -50,16 +42,18 @@ CREATE TABLE Scan
     date          date,
     patient_notes varchar(100),
     patient_id    integer constraint patient_id___fk references patient,
-    time          varchar(100) not null
-
+    time          varchar(10)
 );
 
 CREATE TABLE Surgery (
     surgery_id SERIAL PRIMARY KEY,
     doctor_id INT REFERENCES Doctor(ID),
-    patient_id INT REFERENCES Patient(patient_id),
+    patient_id INT REFERENCES Patient(ID),
     type VARCHAR(50),
     date DATE,
     room_number INT,
-    duration INT
+    hour_minute VARCHAR(50),
+    duration  INT,
+    additional_notes VARCHAR(500),
+    doctor_name VARCHAR(500)
 );
