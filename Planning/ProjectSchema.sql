419d9772858195bd0223beee5f3245cd0832cdbd
Mahmoud <mahmoudmohamedk8200949@gmail.com>
2024-01-04 06:00:29 +0200

    k

--- Planning/ProjectSchema.sql
+++ Planning/ProjectSchema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Doctor (
     ID SERIAL PRIMARY KEY,
-    SSN INT,
+    SSN VARCHAR(30),
     email VARCHAR(100),
     user_name VARCHAR(50) UNIQUE NOT NULL ,
     password VARCHAR(50) NOT NULL ,
@@ -16,44 +16,72 @@ CREATE TABLE Doctor (
     start_work INT,
     end_work INT
     CONSTRAINT chk_gender CHECK(gender IN ('M','F'))
-    CONSTRAINT chk_specialty CHECK (specialty IN ('Orthopedic Surgery', 'Cardiovascular Surgery', 'Neurosurgery'))
     CONSTRAINT chk_department CHECK (department IN ('Radiology', 'Surgery'))
 
 );
 
 CREATE TABLE Patient (
     ID SERIAL PRIMARY KEY,
-    SSN VARCHAR(14),
+    SSN VARCHAR(30),
     email VARCHAR(100) NOT NULL,
     user_name VARCHAR(100) UNIQUE NOT NULL,
-    pass_word VARCHAR(100) NOT NULL ,
+    password VARCHAR(100) NOT NULL ,
     full_name VARCHAR(100),
     birthdate DATE
 );
 
-CREATE TABLE Scan
-(
-    scan_id SERIAL PRIMARY KEY,
-    doctor_id INTEGER REFERENCES doctor(ID),
-    price         integer,
-    machine       varchar(100) not null,
-    category      varchar(100) not null,
-    report        varchar(100),
-    date          date,
-    patient_notes varchar(100),
-    patient_id    integer constraint patient_id___fk references patient,
-    time          varchar(10)
+    CREATE TABLE Scan
+    (
+        scan_id SERIAL PRIMARY KEY,
+        doctor_id INTEGER REFERENCES doctor(ID),
+        price         integer,
+        machine       varchar(100) not null,
+        category      varchar(100) not null,
+        report        varchar(100),
+        date          date,
+        patient_notes varchar(100),
+        patient_id    integer constraint patient_id___fk references patient,
+        time          varchar(10)
+    );
+
+    CREATE TABLE Surgery (
+        surgery_id SERIAL PRIMARY KEY,
+        doctor_id INT REFERENCES Doctor(ID),
+        patient_id INT REFERENCES Patient(ID),
+        type VARCHAR(50),
+        date DATE,
+        room_number INT,
+        duration INT
+    );
+
+
+CREATE TABLE Department(
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR(9),
+    location VARCHAR(50),
+    CONSTRAINT chk_department CHECK (name IN ('Radiology', 'Surgery'))
 );
 
-CREATE TABLE Surgery (
-    surgery_id SERIAL PRIMARY KEY,
+CREATE TABLE WorksIn(
+    department_id INT REFERENCES Department(ID),
     doctor_id INT REFERENCES Doctor(ID),
-    patient_id INT REFERENCES Patient(ID),
-    type VARCHAR(50),
-    date DATE,
-    room_number INT,
-    hour_minute VARCHAR(50),
-    duration  INT,
-    additional_notes VARCHAR(500),
-    doctor_name VARCHAR(500)
+    date_hired DATE
+);
+
+
+
+CREATE TABLE Do (
+    scan_id INT REFERENCES Scan(scan_id),
+    patient_id INT REFERENCES Patient(patient_id),
+    Scan_date DATE
+);
+
+
+CREATE TABLE Admin(
+    ID SERIAL PRIMARY KEY,
+    SSN INT,
+    email VARCHAR(100),
+    password VARCHAR(50) NOT NULL ,
+    user_name VARCHAR(50) UNIQUE NOT NULL ,
+    full_name VARCHAR(50)
 );
